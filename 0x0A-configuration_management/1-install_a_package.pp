de66817374483aaa4e318bde0a1bc0dabbaade4d
Ebele21 <ebele2121@gmail.com>
2023-10-27 13:41:35 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-# It Installs flask from pip3
+# Puppet Manifest to install Flask version 2.1.0 using pip3
+
+# Install Flask version 2.1.0 using pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
