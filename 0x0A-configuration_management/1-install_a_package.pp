6b83634f4c6c61c700029aaea550fa7db0eff974
Ebele21 <ebele2121@gmail.com>
2023-10-28 05:35:50 -0700

    corrected

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,5 +3,10 @@
 # Install Flask version 2.1.0 using pip3
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'werkzeug': # Use a compatible Werkzeug version.
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
